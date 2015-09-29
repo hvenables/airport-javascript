0f9ad60c31e808c98955ffd4fa89de0415c963c3
Harry Venables <venablesh@yahoo.co.uk>
2015-09-29 08:54:54 +0100

    Added stormy weather to airport

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -5,7 +5,10 @@ describe("Airport", function() {
   beforeEach(function() {
     airport = new Airport();
     plane = new Plane();
+    this.isStormy = true;
   });
+
+  // spyOn(airport, 'this.isStormy').and.returnValue(false)
 
   it("should have capacity when created", function() {
     expect(airport.capacity).toBe(50)
@@ -22,5 +25,19 @@ describe("Airport", function() {
     airport.plane_takeoff(plane)
     expect(airport.planes).toEqual([])
     expect(plane.isFlying).toBe(true)
+  });
+
+  it("should be throw an error when no planes", function() {
+    this.isStormy = false;
+    expect(function(){airport.plane_takeoff(plane)})
+    .toThrowError("No Planes to take off")
+  });
+
+  it("should be throw an error when no planes", function() {
+    for (i = 0; i <= airport.capacity; i++) {
+      airport.plane_land(plane)
+    }
+    expect(function(){airport.plane_land(plane)})
+    .toThrowError("Plane cannot take off")
   });
 });
--- src/Airport.js
+++ src/Airport.js
@@ -2,22 +2,32 @@ function Airport(capacity) {
   this.planes = [];
   if (typeof(capacity)==='undefined') capacity = 50;
   this.capacity = capacity;
+  if (Math.floor((Math.random() * 10) + 1) < 2) {
+      this.isStormy = true;
+    } else {
+      this.isStormy = false;
+    }
 };
 
 Airport.prototype.plane_land = function(plane) {
+  if (this.planes.length > this.capacity || this.isStormy) {
+    throw new Error ("Plane cannot take off")
+  }
   plane.land()
   this.planes.push(plane)
 };
 
 Airport.prototype.plane_takeoff = function(plane) {
-
+  if (this.planes.length < 1 || this.isStormy) {
+    throw new Error ("No Planes to take off")
+  }
   plane.takeoff()
-  this.planes.splice(plane)
+  this.planes.pop(plane)
 };
 
-Airport.prototype.isEmpty = function() {
-  return this.planes.length < 1
-}
+
+// Airport.prototype.isEmpty = function() {
+// }
 
 // Airport.prototype.isEmpty = function() {
 //   if (planes === undefined || planes.length == 0)
